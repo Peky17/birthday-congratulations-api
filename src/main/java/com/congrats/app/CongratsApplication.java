39769bfc1f1fc8edafd87d0d274c491f162efd15
git\Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2024-10-31 21:56:51 -0600

    Update main class to accept scheduling events in the app

--- src/main/java/com/congrats/app/CongratsApplication.java
+++ src/main/java/com/congrats/app/CongratsApplication.java
@@ -2,8 +2,10 @@ package com.congrats.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CongratsApplication {
 
     public static void main(String[] args) {
